424bd66fd5de0568773dee4fea362d19d3eebdfd
Ken Ng <ng.ken84@gmail.com>
2018-09-22 16:34:16 -0700

    Add toasts for navigation button pressed

--- app/src/main/java/com/example/ngken/androidbottomnavigationbar/MainActivity.java
+++ app/src/main/java/com/example/ngken/androidbottomnavigationbar/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.ngken.androidbottomnavigationbar;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.main_bottom_nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+
+                    case R.id.menu_home:
+                        Toast.makeText(getApplicationContext(), "HOME", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.menu_map:
+                        Toast.makeText(getApplicationContext(), "MAP", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.menu_done:
+                        Toast.makeText(getApplicationContext(), "DONE", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                }
+
+                return false;
+            }
+        });
     }
 }
